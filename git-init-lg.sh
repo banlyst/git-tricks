ad50946f5b6e1b49f5b8e25ac1382eb95a188fde
Banlyst Yeh <banlyst@gmail.com>
2013-05-21 12:12:31 +0800

    + git lg, git l, git ll, git lp

---
+++ git-init-lg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global alias.l "log --oneline --decorate -13"
+git config --global alias.ll "log --oneline --decorate"
+git config --global alias.lp "log -p"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
