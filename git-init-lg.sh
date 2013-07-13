07ca77aaacd5b2d45f6dea92cfcff1392ec95664
Banlyst Yeh <banlyst@gmail.com>
2013-07-13 16:38:59 +0800

    fixed: "git lg", allow more args like "git lg daily"

--- git-init-lg.sh
+++ git-init-lg.sh
@@ -3,4 +3,5 @@
 git config --global alias.l "log --oneline --decorate -13"
 git config --global alias.ll "log --oneline --decorate"
 git config --global alias.lp "log -p"
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+#git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
